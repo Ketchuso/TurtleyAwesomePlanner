579b9f41b6c8bf2d3fd12b583ddda0049afaa264
Jay Davis <jdlikesbsbl@gmail.com>
2025-04-10 10:46:53 -0500

    adds basic day popup for clicking days

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -4,15 +4,28 @@ import 'react-calendar/dist/Calendar.css';  // Make sure you import the default 
 
 function Calendar() {
     const [date, setDate] = useState(new Date());
+    const [clickedDate, setClickedDate] = useState(null)
+
+    const dateClicked = (value) => {
+        setClickedDate(value);
+    }
 
     return (
         <div className="calendar-container">
             <h1 className="title">My Calendar</h1>
             <ReactCalendar
+                onClickDay={dateClicked}
                 onChange={setDate}
                 value={date}
                 className="custom-calendar"
             />
+            {clickedDate && (
+                <div className="day-box">
+                    You clicked: {clickedDate.toDateString()}
+                </div>
+            )
+
+            }
             <div className="calendar-tail" />
         </div>
     );

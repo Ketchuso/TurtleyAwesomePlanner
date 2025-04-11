0073c87570b3b914c9e4d2f3029a1de859f18c86
Yeji-Yoon <angelayoon0102@gmail.com>
2025-04-11 17:45:17 +0900

    closeout for edit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React, {  useState } from "react";
 import Calendar from "./Calendar";
 import DayDetails from "./DayDetails";
-
+import Edit from "./Edit";
 function App() {
   const [clickedDate, setClickedDate] = useState(null)
+  const [editingEventId, setEditingEventId] = useState(null);
+
+  function setClickedDate_null(){
+    return setClickedDate(null)
+  }
+  
+  function editform_false(){
+    return seteditform(false)
+  }
+  const[editform, seteditform]= useState(false)
+
+  // function setClickedDate_null(){
+  //   return setClickedDate(null)
+  // }
+  function openForm(event_id){
+    seteditform(true)
+    setEditingEventId(event_id)
+  }
+
+  // function edit_event(eventId, updates) {
+  //   fetch(`http://127.0.0.1:5555/events/${eventId}`, {
+  //     method: "PATCH",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(updates),
+  //   })
+  //     .then((r) => r.json())
+  //     .then((updatedEvent) => {
+  //       console.log("Updated event:", updatedEvent);
+  //     });
+  // }
+
 
   return (
     <div className="app">
       <Calendar onDateClick={setClickedDate} />
-      {clickedDate && <DayDetails date={clickedDate} />}
-    </div>
+      {clickedDate && <DayDetails date={clickedDate} setclickeddate_null={setClickedDate_null} openForm={openForm}  />}
+      {editform ? <Edit eventid={editingEventId} editform_false={editform_false}/> : ""}
+      </div>
   );
 }
 
+// of clickDate is true render DayDetails
 export default App;
---
+++ client/src/components/Edit.js
@@ -0,0 +1,70 @@
+import React, {  useState } from "react";
+function Edit({eventid, editform_false}){
+    const[newtitle, setnewtitle]= useState('')
+    const[newdate, setnewdate]=useState('')
+    const[newtime, setnewtime]=useState('')
+
+    function handleSubmit(eventId) {
+        fetch(`http://127.0.0.1:5555/events/${eventId}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: newtitle,
+            date: newdate,
+            time: newtime
+          }),
+        })
+          .then((r) => r.json())
+          .then((updatedEvent) => {
+            console.log("Updated event:", updatedEvent);
+          });
+      }
+
+      return (
+        <div className="form-container">
+          <h1 className="form-title">Edit Event</h1>
+          <button onClick={editform_false} className="delete">🗑️</button>
+          <form className="form-container" onSubmit={(e) => {
+            e.preventDefault()
+            handleSubmit(eventid)
+            }}>
+            <label className="h2.form-title">
+              Title:
+              <input
+                type="text"
+                value={newtitle}
+                onChange={(e) => setnewtitle(e.target.value)}
+                placeholder="Enter title"
+              />
+            </label>
+    
+            <label className="h2.form-title">
+              Date:
+              <input
+                type="text"
+                value={newdate}
+                onChange={(e) => setnewdate(e.target.value)}
+                placeholder="Enter date"
+              />
+            </label>
+    
+            <label className="h2.form-title">
+              Time:
+              <input
+                type="text"
+                value={newtime}
+                onChange={(e) => setnewtime(e.target.value)}
+                placeholder="Enter time"
+              />
+            </label>
+    
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      );
+    }
+    
+    export default Edit;
+    

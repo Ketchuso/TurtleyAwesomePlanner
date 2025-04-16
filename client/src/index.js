925b376fa786fca6e08b99eac27ca869e3511111
Jay Davis <jdlikesbsbl@gmail.com>
2025-04-16 13:12:09 -0500

    adds settings and merges

--- client/src/index.js
+++ client/src/index.js
@@ -1,35 +1,34 @@
 import React from "react";
 import App from "./components/App";
-import Friend from "./components/Friend";
+import Task from "./components/Task";
 import Home from "./components/Home";
+import SettingsPage from "./components/SettingsPage";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SettingsPage from "./components/SettingsPage";
+
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <App />, // layout
+      element: <App />, // layout with NavBar
       children: [
         {
-          path: "/", // this is the calendar page
+          path: "/", // calendar/home
           element: <Home />,
         },
         {
-          path: "/Friend",
-          element: <Friend />,
+          path: "/Task",
+          element: <Task />,
+        },
+        {
+          path: "/Settings/:id", // move SettingsPage in here
+          element: <SettingsPage />,
         },
       ],
     },
-    // Move SettingsPage out of the nested "children" array
-    {
-      path: "/Settings/:id",
-      element: <SettingsPage />,
-    },
   ]);
-
 
 const container = document.getElementById("root");
 const root = createRoot(container);
